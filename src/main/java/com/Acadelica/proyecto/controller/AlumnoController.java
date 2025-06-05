d508b5bdd7a3c6751c61cd03ff0d7817eecc9893
polox2x2 <nekdek1450@gmail.com>
2025-06-04 19:09:01 -0500

    Actualizacion de AlumnosController ,se actualizo el get listar , Ahora utiliza el DTO para no mostrar tanto detalle

--- src/main/java/com/Acadelica/proyecto/controller/AlumnoController.java
+++ src/main/java/com/Acadelica/proyecto/controller/AlumnoController.java
@@ -1,8 +1,10 @@
 package com.Acadelica.proyecto.controller;
 
 import com.Acadelica.proyecto.DTO.Alumno.AlumnoCreateDTO;
+import com.Acadelica.proyecto.DTO.Alumno.AlumnoDTO;
 import com.Acadelica.proyecto.DTO.Alumno.AlumnoDetalleDTO;
 import com.Acadelica.proyecto.DTO.Alumno.AlumnoResponseDTO;
+import com.Acadelica.proyecto.Mappers.AlumnoMappers;
 import com.Acadelica.proyecto.Model.Alumno;
 import com.Acadelica.proyecto.service.AlumnoService;
 import org.springframework.http.ResponseEntity;
@@ -32,10 +34,12 @@ public Alumno crearAlumnos(@RequestBody  Alumno alumno){
     public ResponseEntity<List<Alumno>> listarActivos() {
         return ResponseEntity.ok(alumnoService.listarAlumnosActivos());
     }
-    @GetMapping("/lista")
+   /** @GetMapping("/lista")
     public List<Alumno>listaAlumno(){
         return alumnoService.listaAlumno();
     }
+    */
+
 
 //  CRUD CON DTO
     @PostMapping("/crear")
@@ -43,12 +47,26 @@ public Alumno crearAlumnos(@RequestBody  Alumno alumno){
         AlumnoResponseDTO creado = alumnoService.crearAlumno(dto);
         return ResponseEntity.status(201).body(creado);
     }
+    @GetMapping("/lista")
+    public ResponseEntity<List<AlumnoResponseDTO>>obtenerAlumnoResponse(){
+        List<Alumno>alumnos=alumnoService.listaAlumno();
+        List<AlumnoResponseDTO>responseDTOS = alumnoService.listaAlumno(alumnos);
+        return ResponseEntity.ok(responseDTOS);
+    }
     @GetMapping("/obtener")
     public ResponseEntity<List<AlumnoDetalleDTO>>obtenerAlumnoDetalle(){
         List<Alumno>alumnos = alumnoService.listaAlumno();
         List<AlumnoDetalleDTO> alumnoDetalleDTOS= alumnoService.obtenerDetalleAlumno(alumnos);
         return ResponseEntity.ok(alumnoDetalleDTOS);
     }
+    @GetMapping("/alumnos")
+    public ResponseEntity<List<AlumnoDTO>>AlumnosList(){
+        List<Alumno>alumnos=alumnoService.listaAlumno();
+        List<AlumnoDTO>dto=alumnoService.Alumnos(alumnos);
+        return ResponseEntity.status(201).body(dto);
+    }
+
+
     @GetMapping("/{id}")
     public ResponseEntity<AlumnoDetalleDTO> obtenerAlumnoPorId(@PathVariable Long id) {
         AlumnoDetalleDTO alumno = alumnoService.listarAlumnoPorId(id);
